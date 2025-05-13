efb5cc483c97bcce855ba872d3c76edfef8407c4
Tri <trivu2021@gmail.com>
2025-05-13 01:18:34 -0500

    test new skill method, 03

--- Assets/Scripts/Player/P_Stat.cs
+++ Assets/Scripts/Player/P_Stat.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
+using static UnityEngine.Rendering.DebugUI;
 
 public class P_Stat : MonoBehaviour
 {
@@ -13,20 +15,24 @@ public class P_Stat : MonoBehaviour
     public bool CanMove { get; private set; } = true;
     public bool CanUseSkill { get; private set; } = true;
 
-    public bool LifeSteal { get; private set; }
+    private Buff lifeStealBuff;
     private float lifeStealPercentage;
 
-    private float additionalMoveSpeed;
-    public float MoveSpeed => additionalMoveSpeed + charStat.MoveSpeed;
+    public Buff AdditionalMoveSpeedBuff;
+    private float additionalMoveSpeedPercentage;
+    public float MoveSpeed => charStat.MoveSpeed + (charStat.MoveSpeed * additionalMoveSpeedPercentage / 100);
 
     private float additionalJumpForce;
     public float JumpForce => additionalJumpForce + charStat.JumpForce;
 
-    
+    private float additionalDamage;
+    private float additionalDamagePercentage;
+
+
     private Rigidbody2D rb;
     private Health_Handler health;
     private float orgGravity;
-    private float additionalDamage;
+    
 
     private void Awake()
     {
@@ -56,17 +62,17 @@ public class P_Stat : MonoBehaviour
     }
 
     /* Modifier type */
-    public void Public_Add_MoveSpeed(float value)
-    { additionalMoveSpeed += value; }
+    public void Public_Add_MoveSpeedPercentage(float value)
+    { additionalMoveSpeedPercentage += value; }
     public void Public_Add_JumpForce(float value)
     { additionalJumpForce += value; }
     public void Public_Add_AdditionalDamage(float value)
     { additionalDamage += value; }
+    public void Public_Add_AdditionalDamagePercentage(float value)
+    { additionalDamagePercentage += value; }
 
 
     /* Setter type */
-    public void Public_Set_LifeStealPercentage(float value)
-    { lifeStealPercentage = value; }
     public void Public_ResetGravity()
     { rb.gravityScale = orgGravity; }
 
@@ -74,14 +80,27 @@ public class P_Stat : MonoBehaviour
     /* Action type */
     public void Public_DealDamage(float amount, Health_Handler target)
     {
-        float dmgAmount = amount + additionalDamage;
+        float dmgAmount = (amount + additionalDamage) +
+            (amount + additionalDamage) * additionalDamagePercentage / 100;
         target.Public_DecreaseHealth(dmgAmount); 
-        if(LifeSteal) health.Public_IncreaseHealth(dmgAmount * lifeStealPercentage / 100);
+        if(lifeStealBuff.Active) health.Public_IncreaseHealth(dmgAmount * lifeStealPercentage / 100);
     }
     public void Public_StopMoveOnGround()
     { if (OnGround) rb.linearVelocityX = 0; }
     public void Public_AddForce(Vector2 force, ForceMode2D forceMode)
     { rb.AddForce(force, forceMode); }
+
+    /* Buff type */
+    public void Public_UseBuff_LifeSteal(float duration, float percentage)
+    {
+        lifeStealPercentage = percentage;
+        lifeStealBuff.Public_UseBuff(duration);
+    }
+    public void Public_UseBuff_AddtionalMoveSpeed(float duration, float percentage)
+    { 
+        additionalMoveSpeedPercentage = percentage;
+        AdditionalMoveSpeedBuff.Public_UseBuff(duration);
+    }
 
 
     public void Print(string text)
@@ -92,5 +111,24 @@ public class P_Stat : MonoBehaviour
     {
         Gizmos.color = groundCheckBoxColor;
         Gizmos.DrawWireCube(transform.position + groundCheckBoxOffset, groundCheckBoxSize);
+    }
+}
+public class Buff : MonoBehaviour
+{ 
+    public bool Active;
+    private Coroutine buffCoroutine;
+
+    public void Public_UseBuff(float duration)
+    {
+        if(buffCoroutine != null)
+            StopCoroutine(buffCoroutine);
+
+        buffCoroutine = StartCoroutine(BuffCoroutine(duration));
+    }
+    private IEnumerator BuffCoroutine(float duration)
+    {
+        Active = true;
+        yield return new WaitForSeconds(duration);
+        Active = false;
     }
 }
